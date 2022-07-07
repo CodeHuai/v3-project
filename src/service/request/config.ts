118cd41316a23e68660a40e09a2421e37b54e413
zouweizhe <zouweizhe@guoxintianyu.com>
2022-07-07 12:39:12 +0800

    修改config文件的baseURL

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -1,10 +1,10 @@
-let BASE_URL = ''
+const BASE_URL = 'http://152.136.185.210:5000'
 
 // 生产环境
-if (process.env.NODE_ENV === 'production') {
-  BASE_URL = 'http://152.136.185.210:5000'
-} else {
-  // 开发环境
-  BASE_URL = 'http://152.136.185.210:5000'
-}
+// if (process.env.NODE_ENV === 'production') {
+//   BASE_URL = 'http://152.136.185.210:5000'
+// } else {
+//   // 开发环境
+//   BASE_URL = 'http://152.136.185.210:5000'
+// }
 export default BASE_URL
